b3b600ef45121cf90cf25102dd422b3eb597cd05
Felix Koech Cherop <50258046+fkoech@users.noreply.github.com>
2021-05-21 23:16:36 +0300

    Add files via upload
    a simple applet program

---
+++ LetUsAllLearnJava.java
@@ -0,0 +1,11 @@
+//applet program
+import java.applet.Applet;
+import java.awt.Graphics;
+public class LetUsAllLearnJava extends Applet{
+public static void  main(String[] args){
+}
+public void paint(Graphics g){
+   g.drawString("Dudes should empress java", 40, 20);
+}
+}
+ 
